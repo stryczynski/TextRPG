e61411ea40c816a6d98aa3b2f27a4c409156992c
stryczynski <stryczynski15@gmail.com>
2025-10-31 16:38:13 +0100

    add Player class with attributes and getters/setters

---
+++ src/Player.java
@@ -0,0 +1,58 @@
+public class Player {
+    private String name;
+    private int hp;
+    private int gold;
+    private int level;
+    private int xp;
+    private double attack;
+
+    public double getAttack() {
+        return attack;
+    }
+
+    public void setAttack(double attack) {
+        this.attack = attack;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
